3391dc370ea8246e4f5f34040e2ba0ee532b8eca
Michael <michaelgunawan55@gmail.com>
2023-11-20 13:43:40 +0700

    HTML Admin

--- admin/application/views/login/index.php
+++ admin/application/views/login/index.php
@@ -30,25 +30,4 @@
 
     <!-- Submit button -->
     <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-    <!-- Register buttons -->
-    <!-- <div class="text-center">
-        <p>Not a member? <a href="#!">Register</a></p>
-        <p>or sign up with:</p>
-        <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-facebook-f"></i>
-        </button>
-
-        <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-google"></i>
-        </button>
-
-        <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-twitter"></i>
-        </button>
-
-        <button type="button" class="btn btn-link btn-floating mx-1">
-            <i class="fab fa-github"></i>
-        </button>
-    </div> -->
 </form>
